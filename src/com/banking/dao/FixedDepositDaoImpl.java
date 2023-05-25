7464a45e4a47172ea4cfabed9a6fb57f73b5e567
domce69 <tomm.andraos@gmail.com>
2023-05-25 21:16:52 +1000

    adding functionality

---
+++ src/com/banking/dao/FixedDepositDaoImpl.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FixedDepositDaoImpl implements AccountDao {
+
+	@Override
+	public void assignAccount(Scanner userIn, ArrayList<Customer> customers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void applyInterest(Scanner userIn, ArrayList<Customer> customers) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/com/banking/dto/Customer.java
+++ src/com/banking/dto/Customer.java
@@ -1,7 +1,7 @@
 import java.time.LocalDate;
 
 public class Customer {
-	
+	private static int count = 0;
 	private int id;
 	private String name;
 	private int age;
@@ -11,74 +11,73 @@ public class Customer {
 	private Account acc;
 	
 	
-	public Customer(int id, String name, int age, int mobileNum, String passportNum, LocalDate dOB, Account acc) {
+	public Customer(String name, int age, int mobileNum, String passportNum, LocalDate dOB) {
 		super();
-		this.id = id;
+		this.id = ++count;
 		this.name = name;
 		this.age = age;
 		this.mobileNum = mobileNum;
 		this.passportNum = passportNum;
 		this.DOB = dOB;
-		this.acc = acc;
-
+		//this.acc = acc;
 	}
-
+	
 	public int getId() {
 		return id;
 	}
-
+	
 	public void setId(int id) {
 		this.id = id;
 	}
-
+	
 	public String getName() {
 		return name;
 	}
-
+	
 	public void setName(String name) {
 		this.name = name;
 	}
-
+	
 	public int getAge() {
 		return age;
 	}
-
+	
 	public void setAge(int age) {
 		this.age = age;
 	}
-
+	
 	public int getMobileNum() {
 		return mobileNum;
 	}
-
+	
 	public void setMobileNum(int mobileNum) {
 		this.mobileNum = mobileNum;
 	}
-
+	
 	public String getPassportNum() {
 		return passportNum;
 	}
-
+	
 	public void setPassportNum(String passportNum) {
 		this.passportNum = passportNum;
 	}
-
+	
 	public Account getAcc() {
 		return acc;
 	}
-
+	
 	public void setAcc(Account acc) {
 		this.acc = acc;
 	}
-
+	
 	public LocalDate getDOB() {
 		return DOB;
 	}
-
+	
 	public void setDOB(LocalDate dOB) {
 		DOB = dOB;
 	}
-
+	
 	@Override
 	public String toString() {
 		return "\nCustomer \nID: " + id + "\nName: " + name + "\nAge: " + age + "\nMobile Number: " + mobileNum + "\nPassport Number"
