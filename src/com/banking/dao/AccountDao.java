7672ebdb80d32db5b4f631eb22390cf393e94a30
domce69 <tomm.andraos@gmail.com>
2023-05-25 21:30:25 +1000

    Revert "adding functionality"
    This reverts commit 7464a45e4a47172ea4cfabed9a6fb57f73b5e567.

--- src/com/banking/dao/AccountDao.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
-public interface AccountDao {
-	void assignAccount(Scanner userIn, ArrayList<Customer> customers);
-	void applyInterest(Scanner userIn, ArrayList<Customer> customers);
-}
